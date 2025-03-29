243408229ab6233f564801634ed0be22c6154cb8
jackbenn779 <Jackbenn@ufl.edu>
2025-03-29 11:13:21 -0400

    entree changes

--- Excursion2/exc2.cpp
+++ Excursion2/exc2.cpp
@@ -5,8 +5,23 @@
 #include <vector>
 #include <sstream>
 #include <iomanip>
+#include <bits/stdc++.h>
 
 using namespace std; 
+
+class node
+{
+    ///#this will be !,+,*, or the letter input
+    public:
+    string net;
+    char function; 
+    node* left; 
+    node* right; 
+
+    node(string n){
+        net = n;
+    }
+};
 
 //Create an empty 2d vector with sizes
 vector<vector<double>> create2dVec(int row, int col){
@@ -60,13 +75,52 @@ void outText(string outputStr){
     return; 
 }
 
+node enTree(string line){
 
+    //create the head 
+    int output = line.find("OUTPUT")-2;
+    string out = line.substr(output,line.size()-output);
+
+    cout<<out<<endl;
+
+    node*head = new node(out); 
+
+    head->net = out;
+
+    bool found1st = false;
+    int outGatePos = 0; 
+    for(int  i = 0; i <line.size();i++){
+        if(line[i] == out[0] && !found1st){
+            found1st = true;
+        }
+        else if(line[i] == out[0] && found1st){
+            outGatePos = i; 
+            cout<<"does go here?"<<i<<endl;
+            break;
+        }
+        cout<<"i is:"<<i<<endl;
+    }
+
+    string element = line.substr(outGatePos,line.size() - outGatePos); 
+    cout<<outGatePos<<"element is"<<element; 
+
+    //check to see if out put is a gate or connected to one
+    if(element.find("NOT") == string::npos && element.find("OR") == string::npos &&  element.find("AND") == string::npos){
+        head->left = new node()
+    }
+
+    return head; 
+}
 
 int main(){
 
     //get the string form the file 
-    string line = getText();
+    //string line = getText();
     //Count the number of rows
+
+    string line = "a INPUT b INPUT c INPUT d INPUT E OUTPUT t1 = AND a b t2 = AND c d E = OR t1 t2";
+
+    node head = enTree(line);
 
     outText("12");
 
